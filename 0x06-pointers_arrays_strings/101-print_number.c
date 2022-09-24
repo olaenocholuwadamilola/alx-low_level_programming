08246b74554594346ab0d63e3cf5e682d86c5a11
Ola Enoch <87009421+olaenocholuwadamilola@users.noreply.github.com>
2022-09-24 01:40:23 +0100

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,12 +9,12 @@ void print_number(int n)
 unsigned int num = n;
 if (n < 0)
 {
-_putchar('-');
-num = -num;
+  _putchar('-');
+  num = -num;
 }
 if (num > 9)
 {
-print_number(num / 10);
+  print_number(num / 10);
 }
-_putchar(num % 10 + '0');
+  _putchar(num % 10 + '0');
 }
